284716fc6237224d74663ba2c43d3f99f66eca26
NetoPedro <pedroneto_09@hotmail.com>
2019-08-17 07:59:12 +0100

    [AlexNet][DataLoader] Data Loader now informs the number of classes present in a specific dataset.

--- AlexNet/code/data_loader.py
+++ AlexNet/code/data_loader.py
@@ -2,6 +2,7 @@ import torchvision
 import torch
 transformer =  torchvision.transforms.Compose(
     [torchvision.transforms.ToTensor(),
+     torchvision.transforms.Resize(224),
      torchvision.transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
 
 CIFAR10_train = torchvision.datasets.CIFAR10("../datasets/CIFAR10/", train=True, transform=transformer, target_transform=None, download=True)
@@ -18,21 +19,27 @@ FashionMNIST_test = torchvision.datasets.FashionMNIST("../datasets/FashionMNIST/
 def get_loaders(dataset = "CIFAR10"):
     train_loader = None
     test_loader = None
+    labels_num = None
 
     if dataset == "CIFAR10":
         train_loader = torch.utils.data.DataLoader(CIFAR10_train, batch_size=4,
                                           shuffle=True, num_workers=2)
         test_loader = torch.utils.data.DataLoader(CIFAR10_test, batch_size=4,
                                           shuffle=True, num_workers=2)
+        labels_num = len(set(CIFAR10_train.train_labels))
     elif dataset == "CIFAR100":
         train_loader = torch.utils.data.DataLoader(CIFAR100_train, batch_size=4,
                                                    shuffle=True, num_workers=2)
         test_loader = torch.utils.data.DataLoader(CIFAR100_test, batch_size=4,
                                                   shuffle=True, num_workers=2)
+        labels_num = len(set(CIFAR100_train.train_labels))
+
     elif dataset == "FASHION_MNIST":
         train_loader = torch.utils.data.DataLoader(FashionMNIST_train, batch_size=4,
                                                    shuffle=True, num_workers=2)
         test_loader = torch.utils.data.DataLoader(FashionMNIST_test, batch_size=4,
                                                   shuffle=True, num_workers=2)
+        labels_num = len(set(FashionMNIST_train.train_labels))
 
-    return train_loader,test_loader
+
+    return train_loader,test_loader,labels_num
