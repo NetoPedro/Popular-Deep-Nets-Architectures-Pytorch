391c008c6c77ea5e33221f1ecfdc5a16b8030142
NetoPedro <pedroneto_09@hotmail.com>
2019-08-17 02:51:47 +0100

    [AlexNet][DataLoader] Implemented method to return data loaders for CIFAR10, CIFAR100 and Fashion MNIST

---
+++ AlexNet/code/data_loader.py
@@ -0,0 +1,38 @@
+import torchvision
+import torch
+transformer =  torchvision.transforms.Compose(
+    [torchvision.transforms.ToTensor(),
+     torchvision.transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))])
+
+CIFAR10_train = torchvision.datasets.CIFAR10("../datasets/CIFAR10/", train=True, transform=transformer, target_transform=None, download=True)
+CIFAR100_train = torchvision.datasets.CIFAR100("../datasets/CIFAR100/", train=True, transform=transformer, target_transform=None, download=True)
+FashionMNIST_train = torchvision.datasets.FashionMNIST("../datasets/FashionMNIST/", train=True, transform=transformer, target_transform=None, download=True)
+
+
+
+
+CIFAR10_test = torchvision.datasets.CIFAR10("../datasets/CIFAR10/", train=False, transform=transformer, target_transform=None, download=True)
+CIFAR100_test = torchvision.datasets.CIFAR100("../datasets/CIFAR100/", train=False, transform=transformer, target_transform=None, download=True)
+FashionMNIST_test = torchvision.datasets.FashionMNIST("../datasets/FashionMNIST/", train=False, transform=transformer, target_transform=None, download=True)
+
+def get_loaders(dataset = "CIFAR10"):
+    train_loader = None
+    test_loader = None
+
+    if dataset == "CIFAR10":
+        train_loader = torch.utils.data.DataLoader(CIFAR10_train, batch_size=4,
+                                          shuffle=True, num_workers=2)
+        test_loader = torch.utils.data.DataLoader(CIFAR10_test, batch_size=4,
+                                          shuffle=True, num_workers=2)
+    elif dataset == "CIFAR100":
+        train_loader = torch.utils.data.DataLoader(CIFAR100_train, batch_size=4,
+                                                   shuffle=True, num_workers=2)
+        test_loader = torch.utils.data.DataLoader(CIFAR100_test, batch_size=4,
+                                                  shuffle=True, num_workers=2)
+    elif dataset == "FASHION_MNIST":
+        train_loader = torch.utils.data.DataLoader(FashionMNIST_train, batch_size=4,
+                                                   shuffle=True, num_workers=2)
+        test_loader = torch.utils.data.DataLoader(FashionMNIST_test, batch_size=4,
+                                                  shuffle=True, num_workers=2)
+
+    return train_loader,test_loader
