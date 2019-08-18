dd6fe1b2e4e49f90700496af2922d292d1b55ab1
NetoPedro <pedroneto_09@hotmail.com>
2019-08-18 17:11:00 +0100

    [AlexNet][Train] Added Accuracy computation for test set

--- AlexNet/code/trainer.py
+++ AlexNet/code/trainer.py
@@ -24,11 +24,26 @@ def train(net,trainloader,testloader,optim_name = "adam"):
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-
-            running_loss += loss.item()
             print(i)
-            if i % 100 == 99:  # print every 100 mini-batches
+            running_loss += loss.item()
+            if i % 200 == 199:  # print every 100 mini-batches
                 print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 100))
+                      (epoch + 1, i + 1, running_loss / 200))
                 losses.append(running_loss)
                 running_loss = 0.0
+
+        accuracy = compute_accuracy(net,testloader)
+        print('Accuracy of the network on the test images: %.3f' % accuracy)
+
+def compute_accuracy(net, testloader):
+    net.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in testloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return correct / total
