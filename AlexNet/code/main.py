8658bcc25d8d3fe292a9e3f3fb709e0ef996e389
NetoPedro <pedroneto_09@hotmail.com>
2019-08-20 16:10:35 +0100

    [AlexNet][Main] Added method to run the project on the 3 datasets

--- AlexNet/code/main.py
+++ AlexNet/code/main.py
@@ -2,14 +2,33 @@ import data_loader
 import trainer
 import network
 import torch
+import matplotlib.pyplot as mlp
+import numpy as np
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
-trainloader,testloader,num_classes = data_loader.get_loaders("FASHION_MNIST")
+def run(dataset = "CIFAR10"):
+    trainloader, testloader, num_classes = data_loader.get_loaders(dataset)
 
-net = network.AlexNet(num_classes)
-net.to(device)
+    net = network.AlexNet(num_classes)
+    net.to(device)
 
-trainer.train(net,trainloader,testloader)
+    accuracies, losses = trainer.train(net, trainloader, testloader)
+
+    x = np.linspace(0, 1, len(losses))
+
+    mlp.plot(losses, x)
+    mlp.show()
+
+    x = np.linspace(0, 1, len(accuracies))
+
+    mlp.plot(losses, accuracies)
+    mlp.show()
+
+run()
+run("CIFAR100")
+run("FASHION_MNIST")
+
+
