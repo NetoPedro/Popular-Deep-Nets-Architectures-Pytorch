7bac404c4bf5f39af0772c0e4b52eb146555be45
NetoPedro <pedroneto_09@hotmail.com>
2019-09-03 13:00:12 +0300

    [VGG][Network] Created the network class

---
+++ VGG/code/network.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+
+
+
+class VGG(nn.Module):
+    def __init__(self):
+        super(VGG,self).__init__()
+
+
+
+
+    def forward(self, x):
+
+
+        return x
